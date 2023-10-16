52993091b7558ecf6a13e7b359b632017138ca34
Nina Woesten <105288781+NinaWoesten@users.noreply.github.com>
2023-10-16 09:30:36 +0200

    Create test_csv.py

---
+++ tests/test_csv.py
@@ -0,0 +1,12 @@
+import unittest
+#Funktionen aus helper.py importieren
+from helper import add, get_all, get, delete_all, update
+
+class TestCSVDownload(unittest.TestCase):
+
+    def test_csv(self):
+         sample_data = [
+            {"title": "Item 1", "category": "Category A", "description": "Description 1"},
+            {"title": "Item 2", "category": "Category B", "description": "Description 2, mit Komma},
+        ]
+ csv_string = helper.get_csv(sample_data)
